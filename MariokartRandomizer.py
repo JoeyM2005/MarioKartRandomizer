bf7864f64ad3455f8fffc0a83d8f9bf1489ae2eb
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2025-02-28 06:08:27 -0500

    Update MariokartRandomizer.py
    Added a while loop for a play again thing. Keep it if you want. I'm gonna also make an array of all the players corresponding to their row and col position and have it so that it prints out the character name rather than the row and col

--- MariokartRandomizer.py
+++ MariokartRandomizer.py
@@ -6,6 +6,7 @@ random.seed(time.time())
 KART = 40
 WHEEL = 21
 GLIDE = 15
+PLAYERS = []
 
 CHAR_ROW = 6
 CHAR_COL = 8
@@ -15,58 +16,63 @@ rand_items = random.randint(1,7)
 ITEM_ROW = 4
 ITEM_COL = 6
 
-players = int(input("How many players? (MAX of 4) "))
+playChoice = input("Would you like to play? (YES/NO)")
+while(playChoice.lower() == "yes"):
 
-COURSES = int(input("How many Courses? (MAX of 96) "))
-COURSE_ROW = 4
-COURSE_COL = 6
-
-print('\nCHARACTER')
-for i in range(players):
-    row = random.randint(1, CHAR_ROW)
-    if row == 6:
-        col = random.randint(1, CHAR_COL - 1)
-    else:
-        col = random.randint(1, CHAR_COL)
-    print(f'Player {i+1}:\n\tROW, COL: {row}, {col}')
-
-print('\nKART')
-for i in range(players):
-    print(f'Player {i+1}: {random.randint(1, KART)}')
-
-print('\nWHEEL')
-for i in range(players):
-    print(f'Player {i+1}: {random.randint(1, WHEEL)}')
-
-print('\nGLIDE')
-for i in range(players):
-    print(f'Player {i+1}: {random.randint(1, GLIDE)}')
-
-print('\nITEMS')
-for i in range(rand_items):
-    loopRow = random.randint(1, ITEM_ROW)
-    if loopRow == 6:
-        loopRow = random.randint(1, ITEM_COL - 2)
-    else:
-        loopCol = random.randint(1, ITEM_COL)
+    players = int(input("How many players? (MAX of 4) "))
+    
+    COURSES = int(input("How many Courses? (MAX of 96) "))
+    COURSE_ROW = 4
+    COURSE_COL = 6
+    
+    print('\nCHARACTER')
+    for i in range(players):
+        row = random.randint(1, CHAR_ROW)
+        if row == 6:
+            col = random.randint(1, CHAR_COL - 1)
+        else:
+            col = random.randint(1, CHAR_COL)
+        print(f'Player {i+1}:\n\tROW, COL: {row}, {col}')
+    
+    print('\nKART')
+    for i in range(players):
+        print(f'Player {i+1}: {random.randint(1, KART)}')
+    
+    print('\nWHEEL')
+    for i in range(players):
+        print(f'Player {i+1}: {random.randint(1, WHEEL)}')
+    
+    print('\nGLIDE')
+    for i in range(players):
+        print(f'Player {i+1}: {random.randint(1, GLIDE)}')
+    
+    print('\nITEMS')
+    for i in range(rand_items):
+        loopRow = random.randint(1, ITEM_ROW)
+        if loopRow == 6:
+            loopRow = random.randint(1, ITEM_COL - 2)
+        else:
+            loopCol = random.randint(1, ITEM_COL)
+            
+        print(f'\tITEM {i+1} ROW, COL: {loopRow}, {loopCol}')
         
-    print(f'\tITEM {i+1} ROW, COL: {loopRow}, {loopCol}')
+    print('\nCOURSE')
+    course = random.randint(1,4)
+    course_row = random.randint(1, COURSE_ROW)
+    course_col = random.randint(1, COURSE_COL)
     
-print('\nCOURSE')
-course = random.randint(1,4)
-course_row = random.randint(1, COURSE_ROW)
-course_col = random.randint(1, COURSE_COL)
-
-courseList = {1:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
-2:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
-3:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
-4:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]}}
+    courseList = {1:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
+    2:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
+    3:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]},
+    4:{1:[1, 2, 3, 4],2:[1, 2, 3, 4],3:[1, 2, 3, 4],4:[1, 2, 3, 4],5:[1, 2, 3, 4],6:[1, 2, 3, 4]}}
+        
+    for i in range(COURSES):
+        while(course not in courseList[course_row][course_col]):
+            course_row = random.randint(1, COURSE_ROW)
+            course_col = random.randint(1, COURSE_COL)
+            course = random.randint(1,4)
+        courseList[course_row][course_col].remove(course)
+        print(f'\tCOURSE {i+1} ROW, COLUMN, TRACK: {random.randint(1, COURSE_ROW)}, {random.randint(1, COURSE_COL)}, {course}')
     
-for i in range(COURSES):
-    while(course not in courseList[course_row][course_col]):
-        course_row = random.randint(1, COURSE_ROW)
-        course_col = random.randint(1, COURSE_COL)
-        course = random.randint(1,4)
-    courseList[course_row][course_col].remove(course)
-    print(f'\tCOURSE {i+1} ROW, COLUMN, TRACK: {random.randint(1, COURSE_ROW)}, {random.randint(1, COURSE_COL)}, {course}')
-    
+    playChoice = input("Would you like to play? (YES/NO)")
+print("Kill yourself ong")
